48caae8f19e8e2753a5a87558929fb4abd021371
ramboy777 <ramadhanpuspa10@gmail.com>
2025-06-29 17:11:01 +0700

    projek

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:sushi_app/screen/login_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Sushi app',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: LoginScreen(),
+    );
+  }
+}
