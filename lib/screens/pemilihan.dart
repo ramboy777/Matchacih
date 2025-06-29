31bd65353f7c04adde2ed57e2063dea67664b054
gishcaaa <khansagishca@gmail.com>
2025-06-29 17:45:25 +0700

    reservasi-pemilihan

---
+++ lib/screens/pemilihan.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'reservasi.dart';
+
+class SelectionScreen extends StatelessWidget {
+  const SelectionScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Pilihan Pesanan")),
+      body: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            const Text(
+              "DINE IN",
+              textAlign: TextAlign.center,
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 8),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(context, MaterialPageRoute(builder: (_) => RegisterScreen()));
+              },
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.green,
+                padding: const EdgeInsets.symmetric(vertical: 16),
+              ),
+              child: const Text("Pilih DINE IN", style: TextStyle(fontSize: 16)),
+            ),
+            const SizedBox(height: 30),
+            const Text(
+              "TAKE AWAY",
+              textAlign: TextAlign.center,
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 8),
+            ElevatedButton(
+              onPressed: () {
+                //Navigator.push(context, MaterialPageRoute(builder: (_) => const MenuScreen()));
+              },
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.orange,
+                padding: const EdgeInsets.symmetric(vertical: 16),
+              ),
+              child: const Text("Pilih TAKE AWAY", style: TextStyle(fontSize: 16)),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
